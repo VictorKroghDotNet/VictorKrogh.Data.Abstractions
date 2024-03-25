9b1a2d7814a0eecd7d9aa5f528ab1d0c0695b919
KingoBoiii <kingodk@outlook.dk>
2024-03-25 10:07:43 +0100

    Added Unit of Work interface

---
+++ VictorKrogh.Data.Abstractions/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using VictorKrogh.Data.Providers;
+using VictorKrogh.Data.Repositories;
+
+namespace VictorKrogh.Data;
+
+public interface IUnitOfWork : IProviderFactory, IDisposable
+{
+    IsolationLevel IsolationLevel { get; }
+    bool IsCompleted { get; }
+    void Commit();
+    TRepository GetRepository<TRepository>() where TRepository : IRepository;
+}
