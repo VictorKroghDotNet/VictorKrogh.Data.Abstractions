b3a4be3b236c8889d3c01550f6ba999ba292ba3d
KingoBoiii <kingodk@outlook.dk>
2024-03-25 10:10:48 +0100

    Added Unit of Work Factory interface

---
+++ VictorKrogh.Data.Abstractions/IUnitOfWorkFactory.cs
@@ -0,0 +1,8 @@
+﻿using System.Data;
+
+namespace VictorKrogh.Data;
+
+public interface IUnitOfWorkFactory
+{
+    IUnitOfWork CreateUnitOfWork(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted);
+}
